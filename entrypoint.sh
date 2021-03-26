5f9625a8564df587ec4c53beed8cb7ae0a8aedf0
César Gallego Rodríguez <cesar.gallego.next@bbva.com>
2021-03-26 19:44:48 +0100

    fix: ready message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -eu
 
 if [ ! -e /dev/net/tun ]; then
   echo 'FATAL: cannot start ZeroTier One in container: /dev/net/tun not present.'
   exit 1
 fi
 
-echo "Ready" | exec /usr/sbin/zerotier-one
+echo 'Ready'
+exec /usr/sbin/zerotier-one
